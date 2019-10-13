47a0955ca57b76e4b8652d2422b578b61828237c
tjtechi <tjsulaymon@gmail.com>
2019-10-13 19:51:33 +0300

    exc 2.8 completed disable duplicate number

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -3,33 +3,51 @@ import React, { useState } from 'react';
 const App = ({ names }) => {
     const [persons, setPersons] = useState(names);
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
 
-    const Person = ({ name }) => <p>{name}</p>;
-    const personsList = persons.map(person => <Person key={person.id} name={person.name} />);
+    const Person = ({ name, number }) => (
+        <p>
+            {name} {number}
+        </p>
+    );
+    const personsList = persons.map(person => <Person key={person.id} name={person.name} number={person.number} />);
 
-    const handleNameChange = e => {
+    const handleName = e => {
         setNewName(e.target.value);
         // console.log(newName);
+    };
+
+    const handleNumber = e => {
+        setNewNumber(e.target.value);
     };
 
     const handleSubmit = e => {
         e.preventDefault();
         const newPersonObject = {
             id: persons.length + 1,
-            name: newName
+            name: newName,
+            number: newNumber
         };
 
-        const checkPersonName = persons.find(person => person.name.toLowerCase() === newName.toLowerCase());
-        console.log(checkPersonName);
+        const checkPersonObject = persons.find(
+            person => person.name.toLowerCase() === newName.toLowerCase() || person.number === newNumber
+        );
 
-        if (checkPersonName) {
-            alert(`${newName} is already added to phone book`);
+        // console.log(checkPersonObject);
+
+        if (checkPersonObject) {
+            console.log(checkPersonObject.name.toLowerCase());
+            if (checkPersonObject.name.toLowerCase() === newName.toLowerCase())
+                alert(`${newName} is already added to phone book`);
+            if (checkPersonObject.number === newNumber) alert(`${newNumber} is already added to phone book`);
             setNewName('');
+            setNewNumber('');
             return;
         }
         setPersons(persons.concat(newPersonObject));
 
         setNewName('');
+        setNewNumber('');
     };
 
     return (
@@ -37,7 +55,10 @@ const App = ({ names }) => {
             <h2>Phone book</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    name: <input value={newName} onChange={handleNameChange} />
+                    Name: <input value={newName} onChange={handleName} />
+                </div>
+                <div>
+                    Number: <input value={newNumber} onChange={handleNumber} />
                 </div>
                 <div>
                     <button type="submit">add</button>
--- part_2/phonebook/src/index.js
+++ part_2/phonebook/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const names = [{ id: 1, name: 'Suly Teejay' }, { id: 2, name: 'John Doe' }, { id: 3, name: 'Mango Papaya' }];
+const names = [
+    { id: 1, name: 'Suly Teejay', number: '040-1237854' },
+    { id: 2, name: 'John Doe', number: '040-1689594' },
+    { id: 3, name: 'Mango Papaya', number: '040-2854123' }
+];
 
 ReactDOM.render(<App names={names} />, document.getElementById('root'));
